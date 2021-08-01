8176c4d2e2b11dc1862e2b169f959911ced1e623
daliayan <daliayan5@gmail.com>
2021-08-01 10:55:57 -0400

    Adding callbacks to map/reduce functions to dry up code

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your functions here
+function map(num, callback) {
+    const r = [];
+
+    for (let i = 0; i < num.length; i++ ) {
+        const element = num[i];
+      r.push(callback(element));
+    }
+    return r
+  }
+
+  function reduce(num, callback, start){
+      let total;
+      if (start){
+          total = start;
+          for (let i = 0; i < num.length; i++){
+              total = callback(num[i], total);
+          }
+          return total;
+      } else {
+          total = num[0];
+          for (let i = 1; i < num.length; i++){
+            total = callback(num[i], total);
+      }
+      return total;
+    }
+  }
